1356e6d692555413279ee7fd8b1723935f2548fc
Yghore <yhgore@gmail.com>
2021-01-07 17:19:20 +0100

    1.3.1
    Ajout d'un footer un petit peu mieux

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer fixed-bottom mt-auto py-3 bg-dark">
     <div class="container">
-      <span style="color: white;">AmazonTesting | Crée pour Rémi Vidon par <a href="#">Yghore</a></span>
+      <span style="color: white;">{{ trans('product.app_name') }} | Crée pour Rémi Vidon par <a href="https://discord.gg/RnbsQ86YTC">Yghore</a></span>
     </div>
   </footer>
